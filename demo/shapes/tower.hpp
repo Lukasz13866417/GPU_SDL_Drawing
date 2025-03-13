4592a89b41566e4d3a6ee3ec4054c54855abfaac
Lukasz13866417 <ls459511@students.mimuw.edu.pl>
2025-03-14 00:37:23 +0100

    differnt base of tower

--- demo/shapes/tower.hpp
+++ demo/shapes/tower.hpp
@@ -4,34 +4,34 @@
 #include "shape3d.hpp"
 
 struct Tower {
-    Shape3D baseCuboid;
+    Shape3D basePrism;
     Shape3D hexPrism;
     Shape3D hexPyramid;
     float rotationSpeed;  
     vec position;
 
-    Tower(float cuboidHeight, float prismHeight, float pyramidHeight,
+    Tower(float basePrismHeight, float prismHeight, float pyramidHeight,
           float radius, float spinSpeed, const vec& pos)
         : rotationSpeed(spinSpeed), position(pos)
     {
-        baseCuboid = createCuboid(radius * 1.2f, radius * 1.2f, cuboidHeight, fromRgb(100,100,100));
+        basePrism = createPrism(5, radius * 1.4f, basePrismHeight, fromRgb(100,100,100));
         hexPrism   = createPrism(10,radius, prismHeight, fromRgb(51,51,51));
         hexPyramid = createPyramid(10,radius, pyramidHeight, fromRgb(100,50,50));
 
         for (auto &v : hexPrism.vertices) {
-            v.y += cuboidHeight + 1;
+            v.y += basePrismHeight + 1;
         }
         for (auto &v : hexPyramid.vertices) {
-            v.y += (cuboidHeight + prismHeight + 1);
+            v.y += (basePrismHeight + prismHeight + 1);
         }
 
-        for (auto &v : baseCuboid.vertices)  { v = v + pos; }
+        for (auto &v : basePrism.vertices)  { v = v + pos; }
         for (auto &v : hexPrism.vertices)    { v = v + pos; }
         for (auto &v : hexPyramid.vertices)  { v = v + pos; }
     }
 
     void update() {
-        /*for (auto &v : baseCuboid.vertices) {
+        /*for (auto &v : basePrism.vertices) {
             v = rotY(v, position, rotationSpeed);
         }
         for (auto &v : hexPrism.vertices) {
@@ -47,7 +47,7 @@ struct Tower {
               float cameraYaw,
               float cameraPitch)
     {
-        drawShape(depthBuffer, baseCuboid, cameraPos, cameraYaw, cameraPitch);
+        drawShape(depthBuffer, basePrism, cameraPos, cameraYaw, cameraPitch);
         drawShape(depthBuffer, hexPrism,   cameraPos, cameraYaw, cameraPitch);
         drawShape(depthBuffer, hexPyramid, cameraPos, cameraYaw, cameraPitch);
     }
